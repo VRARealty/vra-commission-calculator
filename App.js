2c77d17f6d5ef8d7925ab63546057aad0aa08810
VRARealty <erica@vrarealty.com>
2025-04-07 13:54:44 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CommissionCalculator from "./CommissionCalculator";
+
+function App() {
+  return (
+    <div className="App">
+      <CommissionCalculator />
+    </div>
+  );
+}
+
+export default App;
